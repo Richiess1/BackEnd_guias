d646de565a9759cabdf1ef9fe0a290b0900aaea8
Richiess1 <20235894@esen.edu.sv>
2025-02-13 12:43:26 -0600

    Funciones

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,14 +13,17 @@ class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
+     * Muestra todos los usuarios en formato JSON.
      */
     public function index()
     {
-        //
+        $users = User::all();
+        return response()->json(UserResource::collection($users));
     }
 
     /**
      * Store a newly created resource in storage.
+     * Crea un nuevo usuario con una contraseña generada aleatoriamente.
      */
     public function store(StoreUserRequest $request)
     {
@@ -29,35 +32,37 @@ class UserController extends Controller
 
         $user = User::create($data);
 
-        return response()->json(UserResource::make( $user ) );
-
-        
+        return response()->json(UserResource::make($user), 201);
     }
 
     /**
      * Display the specified resource.
+     * Muestra un usuario específico.
      */
     public function show(User $user)
     {
-        //
+        return response()->json(UserResource::make($user));
     }
 
     /**
      * Update the specified resource in storage.
+     * Actualiza los datos de un usuario.
      */
     public function update(UpdateUserRequest $request, User $user)
     {
         $data = $request->validated();
         $user->update($data);
 
-        return response()->json(UserResource::make( $user ) );
+        return response()->json(UserResource::make($user));
     }
 
     /**
      * Remove the specified resource from storage.
+     * Elimina un usuario.
      */
     public function destroy(User $user)
     {
-        //
+        $user->delete();
+        return response()->json(['message' => 'Usuario eliminado correctamente'], 200);
     }
 }
